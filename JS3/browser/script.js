dabcbb3ab2d6272d227ccc5f316c75dce79c1cf6
OnlineProjectsGit <emtech.projects.live@gmail.com>
2024-05-27 14:26:20 +1000

    Updates on JS3. Added JS4

--- JS3/browser/script.js
+++ JS3/browser/script.js
@@ -1,3 +1,8 @@
 import sayHello from "./module.js";
 
 sayHello()
+
+let body = document.querySelector('body')
+let heading = document.querySelector('.heading')
+let headingContent = heading.textContent
+
---
+++ JS4/asset/script.js
@@ -0,0 +1,61 @@
+// BIND WINDOW HEIGHT AND WIDTH TO VARIABLES
+let height = window.innerHeight
+let width = window.innerWidth
+console.log(`Height: ${height}\nWidth: ${width}`)
+
+// RETURN HTML COLLECTION (ARRAY OF ELEMENTS)
+let heading = document.getElementsByClassName('heading')
+heading[0].textContent = "Hello Team!"
+// console.log(heading)
+
+// RETURN HTML COLLECTION (ARRAY OF ELEMENTS) AND LOOP THROUGH
+let li = document.getElementsByClassName('list-item')
+for (let i = 0; i < li.length; i++) {
+    const element = li[i];
+    // console.log(element.className)
+}
+// console.log(li)
+
+// CREATE A NEW CHILD NODE, ADD TEXT AND appendChild TO DOM
+let myText = "We had a great weekend with the kids"
+let p = document.createElement('p')
+p.innerText = myText
+// document.body.appendChild(p)
+
+// SELECT SINGLE ELEMENT USING SELECTOR
+let hiBtn = document.querySelector('.hi-btn')
+function sayHi() {
+    alert("hello team!")
+}
+// hiBtn.addEventListener('click', sayHi)
+// hiBtn.addEventListener('click', () =>
+// alert("hello team!")
+// )
+hiBtn.addEventListener('click', sayHi)
+// console.log(hiBtn)
+
+
+// SELECT MULTIPLE ELEMENTS USING SELECTOR AND LOOP
+let liQuerry = document.querySelectorAll('.list-item')
+for (let i = 0; i < liQuerry.length; i++) {
+    const element = liQuerry[i];
+    element.addEventListener('mouseover', (event) => {
+        let target = event.target
+        target.style.backgroundColor = "orange"
+    })
+    element.addEventListener('mouseout', (event)=>{
+        let target = event.target
+        target.style.backgroundColor = "white"
+    })
+}
+
+console.log(liQuerry)
+
+// function runThisFunction()
+
+// myEvent.addEvetListener('click' , (event,name)=>{
+    // let target = event.target
+    // let myObj = {
+    //     name: name
+    // }
+// }))
